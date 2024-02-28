e7ecc70f4301f31eabcff8cb7ef0883ca24f52f6
lexor88 <maksimleksin88@gmail.com>
2024-02-28 14:09:26 +0200

    git commit -m "Added main.py to project and called add function"

---
+++ main.py
@@ -0,0 +1,7 @@
+from funcs import add, subtract
+
+result_addition = add(5, 3)
+result_subtraction = subtract(10, 4)
+
+print("Result of addition:", result_addition)
+print("Result of subtraction:", result_subtraction)
